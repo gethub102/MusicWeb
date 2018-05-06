e22253c81f9cb439ee3606a854e0301cef5b88ee
Steven <sunetwli102@gmail.com>
2018-05-06 14:06:56 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {SearchComponent} from './search/search.component';
+import {FavoriteComponent} from './favorite/favorite.component';
+
+ const routers: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'favorite', component: FavoriteComponent},
+ ];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    FavoriteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routers)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
