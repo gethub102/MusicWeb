a77b5a98e3f807e2174eeecd0867ba053f91bdd6
Steven <sunetwli102@gmail.com>
2018-05-12 00:40:03 -0500

    pipe original

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { HomeComponent } from './home/home.component';
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
